790d513d7155a35f7af79371d7d6607e1b6ecc54
imranN74 <imrannaziransari69@gmail.com>
2024-10-22 01:21:12 +0530

    implemented delete, update, get all applications routes in jobapp

--- backend/src/routes/jobapp.ts
+++ backend/src/routes/jobapp.ts
@@ -7,10 +7,33 @@ import {
 } from "@imrannazir/joblytics-zod";
 import { PrismaClient } from "@prisma/client";
 import userAuthorization from "../middleware/user";
-import { stat } from "fs";
 
 const prisma = new PrismaClient();
 const router = Router();
+
+//get all job application
+router.get("/", userAuthorization, async (req: Request, res: Response) => {
+  const userid = res.locals.userId;
+  try {
+    const response = await prisma.jobApplication.findMany({
+      where: {
+        isActive: true,
+        userId: userid,
+      },
+    });
+    if (response) {
+      res.status(statusCode.accepted).json({
+        data: response,
+        message: "applications fetched",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(statusCode.serverError).json({
+      message: "error while fetching applications!",
+    });
+  }
+});
 
 //create new job application
 router.post(
@@ -31,6 +54,7 @@ router.post(
         res.status(statusCode.badRequest).json({
           message: "invalid input",
         });
+        return;
       }
 
       if (validateData.success) {
@@ -62,9 +86,83 @@ router.post(
 );
 
 //delete job application
-router.post("/delete", (req: Request, res: Response) => {});
+router.post("/delete/:id", async (req: Request, res: Response) => {
+  const validateData = deleteJobApplication.safeParse(req.params.id);
+
+  try {
+    if (validateData.error) {
+      res.status(statusCode.notFound).json({
+        message: "job application doesn't exists",
+      });
+      return;
+    }
+    if (validateData.success) {
+      const { jobId } = validateData.data;
+      await prisma.jobApplication.update({
+        data: {
+          isActive: false,
+        },
+        where: {
+          id: jobId,
+        },
+      });
+      res.status(statusCode.accepted).json({
+        message: "application deleted",
+      });
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(statusCode.serverError).json({
+      message: "errro while deleting,try again",
+    });
+  }
+});
 
 //update job application
-router.post("/update", (req: Request, res: Response) => {});
+router.post(
+  "/update/:id",
+  userAuthorization,
+  async (req: Request, res: Response) => {
+    const validateData = updateJobApplication.safeParse(req.body);
+    const jobId = req.params.id;
+
+    if (validateData.error) {
+      const path = validateData.error.issues[0].path[0];
+      if (path === "appliedDate") {
+        res.status(statusCode.badRequest).json({
+          message: "invalid date format, Expected YYYY-MM-DD",
+        });
+        return;
+      }
+      res.status(statusCode.badRequest).json({
+        message: "invalid input",
+      });
+      return;
+    }
+
+    if (validateData.success) {
+      const { company, role, appliedDate, applicationStatus } =
+        validateData.data;
+      const response = await prisma.jobApplication.update({
+        data: {
+          company: company,
+          role: role,
+          appliedDate: new Date(appliedDate),
+          appStatus: applicationStatus,
+        },
+        where: {
+          id: jobId,
+        },
+      });
+      // console.log(response);
+      res.status(statusCode.created).json({
+        data: response,
+        message: "appliction added",
+      });
+      return;
+    }
+  }
+);
 
 export default router;
