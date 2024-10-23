f1de30c20cc04deb0778aea0af49f73b122df8ff
imranN74 <imrannaziransari69@gmail.com>
2024-10-24 02:31:02 +0530

    made layout changes on application page

--- client/src/components/AppContainer.tsx
+++ client/src/components/AppContainer.tsx
@@ -2,21 +2,29 @@ import { DeleteButton } from "./DeleteButton";
 import { EditButton } from "./EditButton";
 import { StatusDropDown } from "./StatusDropDown";
 import { AppValues } from "./AppValues";
+import { NotesButton } from "./NotesButton";
 
 export const AppContainer = () => {
   return (
     <div className="border rounded-xl md:px-5 shadow-md mt-2">
       <div>
-        <div className="flex justify-end px-5 py-2">
-          <DeleteButton />
-          <EditButton />
+        <div className="flex justify-between px-5 py-2">
+          <div>
+            <NotesButton />
+          </div>
+          <div>
+            <StatusDropDown />
+          </div>
+          <div className="flex">
+            <DeleteButton />
+            <EditButton />
+          </div>
         </div>
         <div className="flex justify-around items-center py-5">
           <AppValues value="Google" />
           <AppValues value="SDE-1" />
           <AppValues value="Banglore" />
           <AppValues value="2 Oct,2024" />
-          <StatusDropDown />
         </div>
       </div>
     </div>
--- client/src/components/StatusDropDown.tsx
+++ client/src/components/StatusDropDown.tsx
@@ -1,11 +1,49 @@
+import { useEffect, useState } from "react";
+
 export const StatusDropDown = () => {
+  const [bgColor, setBgColor] = useState("");
+
+  function handleColorChange(value: string) {
+    switch (value) {
+      case "applied":
+        setBgColor("bg-yellow-400");
+        break;
+      case "rejected":
+        setBgColor("bg-red-400");
+        break;
+      case "interview":
+        setBgColor("bg-green-400");
+        break;
+      case "neglect":
+        setBgColor("bg-slate-500");
+        break;
+      default:
+        setBgColor("");
+    }
+  }
+
+  function handleStatusChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const { value } = event.target;
+    handleColorChange(value);
+  }
+
+  useEffect(() => {
+    handleColorChange("applied");
+  }, []);
+
   return (
-    <div className="font-semibold border border-black rounded-md md:px-2">
-      <select title="status" className="focus:outline-none">
-        <option>Applied</option>
-        <option>Interview</option>
-        <option>Rejected</option>
-        <option>Neglect</option>
+    <div
+      className={`font-semibold border ${bgColor} border-black rounded-md md:px-2`}
+    >
+      <select
+        title="status"
+        className={`focus:outline-none w-32 py-2 ${bgColor}`}
+        onChange={handleStatusChange}
+      >
+        <option value={"applied"}>Applied</option>
+        <option value={"interview"}>Interview Scheduled</option>
+        <option value={"rejected"}>Rejected</option>
+        <option value={"neglect"}>Neglect</option>
       </select>
     </div>
   );
