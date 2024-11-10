69f7c090cb46eb306d98d820ae4f845141514642
imranN74 <imrannaziransari69@gmail.com>
2024-11-11 00:49:26 +0530

    add about page correction

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -4,7 +4,7 @@ import { ContactLandContainer } from "../components/landing/ContactLandContainer
 
 export const About = () => {
   return (
-    <div className="mt-28 sm:mt-16">
+    <div className="mt-28 sm:mt-16 bg-gradient-to-bl from-cyan-50 to-cyan-100">
       <AppLandContainer />
       <AnalyticsLandContainer />
       <ContactLandContainer />
